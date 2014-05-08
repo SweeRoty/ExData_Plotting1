0d71b1f3f5d1659b40271c999773b67ad63e093e
SweeRoty <sweeroty@gmail.com>
2014-05-08 18:47:34 +0800

    Course Project 1

--- plot1.R
+++ plot1.R
@@ -1,6 +1,5 @@
 data <- read.table("household_power_consumption.txt", header = TRUE, sep = ";")
 data.sub <- data[data$Date == "1/2/2007" | data$Date == "2/2/2007", ]
-#data.sub$Date <- as.Date(data.sub$Date, "%d/%m/%Y")
 data.sub$Global_active_power <- as.character(data.sub$Global_active_power)
 data.sub$Global_active_power <- as.numeric(data.sub$Global_active_power)
 png("plot1.png", width = 480, height = 480, units = "px")
